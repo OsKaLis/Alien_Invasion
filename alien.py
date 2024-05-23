8579b818d4cdd4656b72dcb015c1374af6ea6e40
Юра Юшко <oskaLis@yandex.ru>
2024-05-23 07:57:08 +0300

    23.05.2024 корекция

--- alien.py
+++ alien.py
@@ -3,13 +3,9 @@ from pygame.sprite import Sprite
 
 
 class Alien(Sprite):
-    """
-    Класс, представляющего одного пришельца.
-    """
+    """Класс, представляющего одного пришельца."""
     def __init__(self, ai_settings, screen):
-        """
-        Иницилизируем пришельца и задает его начальную позицию.
-        """
+        """Иницилизируем пришельца и задает его начальную позицию."""
         super().__init__()
         self.screen = screen
         self.ai_settings = ai_settings
@@ -26,15 +22,11 @@ class Alien(Sprite):
         self.x = float(self.rect.x)
 
     def blitme(self):
-        """
-        Выводит пришельца в текущем положении.
-        """
+        """Выводит пришельца в текущем положении."""
         self.screen.blit(self.image, self.rect)
 
     def check_edges(self):
-        """
-        Возвращает True, если пришелец находится у края экрана.
-        """
+        """Возвращает True, если пришелец находится у края экрана."""
         screen_rect = self.screen.get_rect()
         if self.rect.right >= screen_rect.right:
             return True
@@ -42,9 +34,7 @@ class Alien(Sprite):
             return True
 
     def update(self):
-        """
-        Перемещает пришельца влево или вправо.
-        """
+        """Перемещает пришельца влево или вправо."""
         alien_speed_factor = self.ai_settings.alien_speed_factor
         fleet_direction = self.ai_settings.fleet_direction
         self.x += (alien_speed_factor * fleet_direction)
